93b7ca69196a8c62fd6d3a1c59494f23e74f347b
Trevor <trevor.k.gachagua@gmail.com>
2024-10-02 12:06:33 +0300

    first commit

---
+++ volume_of_sphere.cpp
@@ -0,0 +1,20 @@
+// a program to find the volume of a sphere
+#include <iostream>
+using namespace std; 
+
+int main() {
+    float vol;
+    float pi = 3.14;
+    int rad;
+
+    cout << "Enter the radius: ";
+    cin >> rad;
+
+    vol = (4.0 / 3.0) * pi * rad * rad * rad;
+    cout << "The volume of the sphere is: " << vol << endl;
+
+    return 0; 
+}
+
+
+
