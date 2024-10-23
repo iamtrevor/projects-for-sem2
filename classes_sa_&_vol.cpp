9d5d72a5804deb7e6fee47001ddfe996bb088e78
Trevor <trevor.k.gachagua@gmail.com>
2024-10-23 17:32:42 +0300

    first commit

---
+++ classes_sa_&_vol.cpp
@@ -0,0 +1,41 @@
+// a program to calculate volume & surface area
+
+#include <iostream>
+using namespace std;
+
+//function declaration
+float PI = 3.142;
+float volume(int radius, int height);
+float surface_area(int radius, int height);
+
+//cylinder class
+
+class cylinder{
+    public:
+    float volume(int radius, int height){
+        
+        return PI * radius * radius * height;
+    };
+
+    float surface_area(int radius, int height){
+       
+        return  2 * PI * radius * radius + 2 * PI * radius * height;;
+    }
+};
+
+int main(){
+    
+    cylinder cyl;
+
+    int x = 20;
+    int y = 20;
+
+    int sa = cyl.volume(x, y);
+    int vol = cyl.surface_area(x, y);
+
+    cout<<"the volume is"<<sa<<"and the surface area is"<<vol<<endl;
+
+    return 0;
+}
+
+
